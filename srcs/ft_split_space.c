afa833193ef83df9338ee1144bac46d42d2c2e17
m1n-q <m1n_q@naver.com>
2021-10-14 13:17:09 +0900

    issue: nested quote, quote without spacing

--- srcs/ft_split_space.c
+++ srcs/ft_split_space.c
@@ -6,7 +6,7 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 14:12:58 by kyumlee           #+#    #+#             */
-/*   Updated: 2021/10/13 19:49:52 by kyumlee          ###   ########.fr       */
+/*   Updated: 2021/10/14 13:16:26 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ char	**ft_split_space(char const *s, t_cmd *cmd)
 		if (!ft_isspace(*s))
 		{
 			ret[i] = (char *)malloc(sizeof(char) * (cnt_s_len(s, cmd) + 1));
-			if (!ret[i])        /* have to free prev malloced */
+			if (!ret[i])       					/* have to free prev malloced */
 				return (0);
-			if (*s == '"')
+			if (*s == '"' || *s == '\'')			/* add single quote */
 				ft_strlcpy(ret[i], s + 1, cnt_s_len(s, cmd) - 1);
 			else
 				ft_strlcpy(ret[i], s, cnt_s_len(s, cmd) + 1);
