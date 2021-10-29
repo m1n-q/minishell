c34bfc653db33d79ecf8cbf3be65629ef6f0a548
kyumlee <kyumlee@student.42seoul.kr>
2021-10-29 18:03:07 +0900

    a little modification in ft_split_space.c, _space_str.c, _space_cpy.c

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:14:26 by mishin            #+#    #+#             */
-/*   Updated: 2021/10/25 18:27:57 by mishin           ###   ########.fr       */
+/*   Updated: 2021/10/29 17:53:16 by kyumlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ int			print_including_empty(void);
 char		**ft_split_space(char *s);
 
 /* ft_split_space_cpy */
-char		*cpy_str(char *s, char *ret);
+char		*cpy_str(char *s, char **ret, int *i);
 
 /* ft_split_space_env */
 char		*case_env(char *s);
