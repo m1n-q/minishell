1854b5358c04f784d4d6b957bcf767e14bdb4a85
m1n-q <m1n_q@naver.com>
2021-11-08 18:08:28 +0900

    fix duplicated function name

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mishin <mishin@student.42.kr>              +#+  +:+       +#+        */
+/*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 16:24:47 by mishin            #+#    #+#             */
-/*   Updated: 2021/11/08 15:24:20 by kyumlee          ###   ########.fr       */
+/*   Updated: 2021/11/08 18:07:43 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspace(char c)
+int	is_space(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n'
 		|| c == '\v' || c == '\f' || c == '\r');
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 
 	ret = 0;
 	sign = 1;
-	while (ft_isspace(*str))
+	while (is_space(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 		if (*str++ == '-')
