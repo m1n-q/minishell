87984f5dea444629a4046d893fb162a7b919dea5
m1n-q <m1n_q@naver.com>
2021-11-09 18:23:20 +0900

    normintte

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:40:55 by mishin            #+#    #+#             */
-/*   Updated: 2021/11/09 18:01:14 by mishin           ###   ########.fr       */
+/*   Updated: 2021/11/09 18:21:50 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@
 */
 int	__env(char **argv)
 {
-	int			i;
-	t_envent	entry;
+	int	i;
 
 	if (argv[1])
 	{
@@ -46,10 +45,8 @@ int	__env(char **argv)
 	i = -1;
 	while (environ[++i])
 	{
-		entry = find_envent(environ[i]);
-		if (getenv(entry.name))
+		if (getenv(environ[i]))
 			printf("%s\n", environ[i]);
-		free_envent(entry);
 	}
 	return (0);
 }
